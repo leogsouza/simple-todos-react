55bdfa2b2dd9b01b18184ef18b5afdde729ccf6b
Leonardo Souza <leogsouza@gmail.com>
2016-04-26 19:17:40 -0300

    Implementação de tarefa privada

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -6,8 +6,14 @@ export const Tasks = new Mongo.Collection('tasks');
 
 if(Meteor.isServer) {
   // This code only run on the server
+  // Only publish tasks that are public or belong to the current user
   Meteor.publish('tasks', function tasksPublication() {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        {private:  {$ne: true} },
+        {owner: this.userId},
+      ],
+    });
   });
 }
 
@@ -30,6 +36,12 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String);
 
+    const task = Tasks.findOne(taskId);
+    if(task.private && task.owner !== Meter.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked) {
@@ -38,4 +50,18 @@ Meteor.methods({
 
     Tasks.update(taskId, { $set: { checked: setChecked }});
   },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if(task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: {private: setToPrivate } });
+
+  }
 });
